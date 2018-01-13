cad5ab46904a7a7ffd7056cd87fee1f00ce6332c
peerless <598762663@qq.com>
2018-01-14 02:31:29 +0800

    Revert "加入消息"
    This reverts commit 019eacb82a9bc37e7c40ab9f7db7f48c7166d395.

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -62,7 +62,6 @@ BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
-	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication1Dlg::OnBnClickedButton1)
 END_MESSAGE_MAP()
 
 
@@ -151,10 +150,3 @@ HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-
-
-void CMFCApplication1Dlg::OnBnClickedButton1()
-{
-	// TODO:  在此添加控件通知处理程序代码
-	MessageBox(NULL);
-}
